4e81e9784f8f2c783c7e88ee871bb7379cac8cf5
finnbergquist <fbergqui@bowdoin.edu>
2020-06-07 13:12:08 -0400

    changes to step_sequncer

--- Looper/channel_structure.py
+++ Looper/channel_structure.py
@@ -1,7 +1,7 @@
 class channels:
     """multidimensional array representing channels and steps"""
 
-    audio_file_struct = [[5, 0, 5, 0, 5, 0, 5, 0], [3, 0, 0, 0, 3, 0, 0, 0]]
+    audio_file_struct = [["002", "000", "002", "000", "002", "002", "002", "002"], ["000", "000", "000", "000", "000", "000", "000", "000"]]
 
     def __init__(self, tempo,num_channels,num_steps):
         self.tempo = tempo #none of these are really implemented yet
--- Looper/sound_files.py
+++ Looper/sound_files.py
@@ -11,11 +11,11 @@ class mix:
     
     #sounds dictionary
     sounds = {
-        "000" : pygame.mixer.Sound("Audio_Files/Track1.wav"),
         "001" : pygame.mixer.Sound("Audio_Files/Track2.wav"),
         "010" : pygame.mixer.Sound("Audio_Files/drums.wav"),
         "100" : pygame.mixer.Sound("Audio_Files/Track3.wav"),
-        "002" : pygame.mixer.Sound("Audio_Files/2_SECOND_PIANO.wav")
+        "002" : pygame.mixer.Sound("Audio_Files/2_SECOND_PIANO.wav"),
+        "020" : pygame.mixer.Sound("Audio_Files/Track1.wav")
     }
 
     def play(self, sound_code, channel_number):
@@ -27,9 +27,11 @@ class mix:
         if (code in self.sounds):#there is no audio file in this position, so do nothing
             print("sound not in dictionary")
         else:#play specified sound in specified channel
-            # print(type(code))
-            # print(self.sounds.get(code))
-            self.channels[channel_number].play(self.sounds[sound_code])
+            if sound_code == "000":#don't play any sound
+                print("blank sound")
+                return
+            else:
+                self.channels[channel_number].play(self.sounds[sound_code])#play that mf Sound!!
             
     
     def update_channel_volume(self, channel_number, volume):
