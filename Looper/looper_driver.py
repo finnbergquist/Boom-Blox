ba45a6fa432c7cb9ec36f8defe4db86010434ab8
Paul Miller-Schmidt <paulmiller-schmidt@Pauls-MacBook-Pro.local>
2020-06-07 21:17:06 -0400

    Added looper class and updated looper_driver methods

--- Looper/looper_driver.py
+++ Looper/looper_driver.py
@@ -1,42 +1,40 @@
 """Audio Looper driver method"""
 import time
-# from gpiozero import MCP3008, PWMLED
-from channel_structure import channels
-from sound_files import mix
 import serial
-import time
+# from gpiozero import MCP3008, PWMLED
+from looper import Looper
+from sound_files import mix
 
 
-# Set the channel and step clip 
-def set_clip(instr, channels, steps, clip):
-    for i in range(channels):
-        for j in range(steps):
-            instr.set_audio_num(i, j, clip)
+##MIGHT NOT NEED WITH LOOPER METHOD
+# # Set the channel and step clip 
+# def set_clip(instr, channels, steps, clip):
+#     for i in range(channels):
+#         for j in range(steps):
+#             instr.set_audio_num(i, j, clip)
 
     
-def play_region(instr, region_number):
-    """helper method for audio loop"""
-    mixer.play(instr.get_audio_num(0, region_number), 0)
-    mixer.play(instr.get_audio_num(1, region_number), 1)
-    # print(sequencer.get_audio_num(0, region_number))
-    # print(sequencer.get_audio_num(1, region_number))
+def play_region(instr, channel_number):
+    ##play loop from channel number 
+    mixer.play(instr.get_loop(channel_number), channel_number)
 
 
 def start_loop(instr):
     """Executable loop. It updates channel volumes on 0.1 second intervals, and plays
     the next step in the sequence every 2 seconds. Loops after 8 steps"""    
+    ##get number of channels
+    #num_ch = instr.num_channels
     step = -1
     next_time = time.time()
     # mixer.update_channel_volume(0, pot0.value)#setup the channel volumes before audio playback
     # mixer.update_channel_volume(1, pot1.value)
     while True:#audio loop
-        if time.time() >= next_time:
-            step = (step + 1) % 160
-            if (step/20).is_integer():#very fast way to test(i think)
-                play_region(instr, int(step/20))#plays audio files at steps 0,1,2,3,4,5,6,7
-            # mixer.update_channel_volume(0, pot0.value)
-            # mixer.update_channel_volume(1, pot1.value)
-            next_time += 0.1
+        play_region(instr, 0)
+        #Sleep for however long the audio is 
+        time.sleep(1)
+        # if time.time() >= next_time:
+        #     play_region(instr, 0)
+        #     next_time += 1
            
 def sort(input, threshold):
     #sort the serial data into specific channels 
@@ -53,7 +51,7 @@ def sort(input, threshold):
         return "error: can't identify node"
 
 
-
+"""
 # set up the serial line
 port = '/dev/cu.usbmodem143101'
 ser = serial.Serial(port, 9600, timeout=0.1)
@@ -64,9 +62,9 @@ while True:
     #initialize output as 000
     output = "000"
     data = ser.readline()         # read a byte string
-    n = data.decode()  # decode byte string into Unicode 
+    n = data.decode()  # decode byte string into Unicode
     string = n.rstrip() # remove \n and \r
-    # if it is a string we care about 
+    # if it is a string we care about
     if (":" in string):
         #get the resistor value, make it an int, sort it and then 
         #turn it back into a string
@@ -78,33 +76,24 @@ while True:
         
     print(output)
     
-# Accessing sounds: 
-#     each combination will have a 3-digit code. That code comes from
-#     the MIDI, effect and inst inputs. Each of those resistor values
-#     will be sorted into a single digit value from 0-10 with the sort 
-#     function. Then they will be concatanated into a 3-digit code. 
-#     000 representing the default. There will be a dictionary with 
-#     these codes that will inform the Raspi which .wav to play.
-
+"""
 
     #RUN AUDIO LOOP
-
+while True:
     #settig up channel data
-    tempo = 5
-    ch_len= 2
-    steps = 8
-    sequencer = channels(tempo,ch_len,steps)#5 is the tempo, 2 channels, 8 steps
+    TEMPO = 5
+    CHANNELS = 1
+   
+    looper = Looper(TEMPO, CHANNELS)#5 is the tempo, 2 channels, 8 steps
     # sequencer.scan_tracks#not implemented yet
-
+    #output = '001'
     # Set first instrument
-    set_clip(sequencer, ch_len, steps, output) 
-    print(sequencer.audio_file_struct[0])
-    print(sequencer.audio_file_struct[1])
+    looper.set_loop(CHANNELS, '001')
 
-    #setting up mixer 
+    #setting up mixer
     mixer = mix()
     #start loop on sequencer
-    start_loop(sequencer)
+    start_loop(looper)
        
 
             
--- Looper/sound_files.py
+++ Looper/sound_files.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import mixer
+import os
 
 class mix:
     pygame.mixer.pre_init(22050, -16, 2, 256)#22050=default frequecy,-16=size(16 signed bits per audio sample
@@ -10,36 +11,40 @@ class mix:
     channels = [pygame.mixer.Channel(0), pygame.mixer.Channel(1)]
     
     #sounds dictionary
+    # Accessing sounds: 
+#     each combination will have a 3-digit code. That code comes from
+#     the MIDI, effect and inst inputs. Each of those resistor values
+#     will be sorted into a single digit value from 0-10 with the sort 
+#     function. Then they will be concatanated into a 3-digit code. 
+#     000 representing the default. There will be a dictionary with 
+#     these codes that will inform the Raspi which .wav to play.
+
     sounds = {
-        "001" : pygame.mixer.Sound("Audio_Files/Track2.wav"),
-        "010" : pygame.mixer.Sound("Audio_Files/drums.wav"),
-        "100" : pygame.mixer.Sound("Audio_Files/Track3.wav"),
-        "002" : pygame.mixer.Sound("Audio_Files/2_SECOND_PIANO.wav"),
-        "020" : pygame.mixer.Sound("Audio_Files/Track1.wav")
+        "001" : pygame.mixer.Sound("Looper/Audio_Files/Choir.wav"),
+        "010" : pygame.mixer.Sound("Looper/Audio_Files/drums.wav"),
+        "100" : pygame.mixer.Sound("Looper/Audio_Files/Track3.wav"),
+        "002" : pygame.mixer.Sound("Looper/Audio_Files/2_SECOND_PIANO.wav"),
+        "020" : pygame.mixer.Sound("Looper/Audio_Files/Track1.wav")
     }
 
-    def play(self, sound_code, channel_number):
-        """Play arg1 sound in arg2 channel           
-        ex) mix.play(1, 0) will play sound 1 in channel 0"""
-        #make code readable by dict
-        
-        code = "'" + sound_code + "'"
-        if (code in self.sounds):#there is no audio file in this position, so do nothing
-            print("sound not in dictionary")
-        else:#play specified sound in specified channel
-            if sound_code == "000":#don't play any sound
-                print("blank sound")
-                return
-            else:
-                self.channels[channel_number].play(self.sounds[sound_code])#play that mf Sound!!
+    def play(self, sound_code, channel_number): 
+            """Play arg1 sound in arg2 channel           
+            ex) mix.play(1, 0) will play sound 1 in channel 0"""
+            #make code readable by dict
             
-    
+            code = "'" + sound_code + "'"
+            if (code in self.sounds):#there is no audio file in this position, so do nothing
+                print("sound not in dictionary")
+            else:#play specified sound in specified channel
+                # print(type(code))
+                # print(self.sounds.get(code))
+                self.channels[channel_number].play(self.sounds[sound_code])
+                
     def update_channel_volume(self, channel_number, volume):
         """sets the volume of a specific chanel"""     
         self.channels[channel_number].set_volume(volume)
-        
-        
-    
+            
+            
     def cleanup(self):#need to use an exception handler!!!!! in driver
         """called at end of driver"""
         pygame.quit()
