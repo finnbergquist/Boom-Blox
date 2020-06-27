95d2fc162694b34ddb19a34bddebe49a67205402
finnbergquist <fbergqui@bowdoin.edu>
2020-06-27 10:08:06 -0400

    new inputs

--- Looper/channel_structure.py
+++ Looper/channel_structure.py
@@ -1,4 +1,13 @@
 from gpiozero import MCP3008
+import smbus2
+
+bus = smbus2.SMBus(1)
+arduino_address = 0x04
+
+def read()
+    """returns 16 bytes in an array for, each byte corresponding to an analog input from arduino mega"""
+    input = bus.read_i2c_block_data(arduino_address, 0, 16)
+    return input
 
 
 def to_sound_code(resistor_value):
@@ -54,7 +63,7 @@ class channels:
         self.num_channels = num_channels
         self.num_steps = num_steps
         self.audio_file_struct = [["000" for i in range(0,num_steps)] for j in range(0,num_channels)]
-        self.steps_resistance_values = []#holds resistor scanning objects at each step
+        self.steps_resistance_values = []#holds current resistor values
 
     def init_analog_inputs(self):
         """setup the channel resistor reads for each step in a 
@@ -65,7 +74,9 @@ class channels:
     def scan_tracks(self):
         """Fill the step-sequencer array depending on the
         the values yielded by node resistor inputs"""
-        for j in range(0, 2):
+        self.steps_resistance_values = read()#i hope this works
+
+        for j in range(0, self.num_channels):
             print("entering j loop")
             for i in range(0, self.num_steps):#assigning sound_codes based on resistance value in the steps
                 self.audio_file_struct[j][i] = to_sound_code(self.steps_resistance_values[i + (4*j)].value)#only doing this in first channel(for now!!)
