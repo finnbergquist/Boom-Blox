85a4a8b2a61c1a17425e6a8f8ffa04ae877df393
finnbergquist <fbergqui@bowdoin.edu>
2020-07-04 11:44:40 -0400

    Update Control_LED_by_Potentiometer.py

--- Looper/Old_Files/Control_LED_by_Potentiometer.py
+++ Looper/Old_Files/Control_LED_by_Potentiometer.py
@@ -1,9 +1,9 @@
 from gpiozero import MCP3008
 from time import sleep
 
-SPI_PORT   = 0
+SPI_PORT  = 0
 SPI_DEVICE = 0
-mcp = MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+mcp = MCP3008(spi=SpiDev(SPI_PORT, SPI_DEVICE))
 
 pot = MCP3008(0)#channel 0 of the MPC3008 pins
 pot1 = MCP3008(1)#MCP3008 channel 1
