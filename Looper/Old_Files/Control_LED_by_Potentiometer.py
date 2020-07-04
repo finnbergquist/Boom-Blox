b62ec4117a4a9f6771ebaa53bf3b00fab7f965f9
finnbergquist <fbergqui@bowdoin.edu>
2020-07-04 11:40:46 -0400

    Update Control_LED_by_Potentiometer.py

--- Looper/Old_Files/Control_LED_by_Potentiometer.py
+++ Looper/Old_Files/Control_LED_by_Potentiometer.py
@@ -1,5 +1,9 @@
-from gpiozero import MCP3008, PWMLED
+from gpiozero import MCP3008
 from time import sleep
+
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 pot = MCP3008(0)#channel 0 of the MPC3008 pins
 pot1 = MCP3008(1)#MCP3008 channel 1
