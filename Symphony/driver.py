7d47859b051fd4ce893ebde9946c6c6ac0079993
finnbergquist <fbergqui@bowdoin.edu>
2020-07-09 10:34:39 -0400

    Update driver.py

--- Symphony/driver.py
+++ Symphony/driver.py
@@ -3,10 +3,13 @@ from step_sequencer import step_sequencer
 import RPi.GPIO as GPIO
 import smbus2
 import time
+import os
+import signal
 
 #setting up buttons
 GPIO.setmode(GPIO.BCM)#normal gpio sumbering system
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 #setting up i2c bus, bus object passed down to channel_structure to be used for scanning trakcs
 #in the other arduino slave
@@ -27,11 +30,22 @@ def play_step_sequencer():
     #trigger lights and other haptics
     stepSequencer.step_sequencer_loop()
 
+def end_step_sequencer():
+    """kills step sequencer thread"""
+    os.kill(p.pid,signal.SIGINT)
+
+def signal_handler(frame):
+    mixer.cleanup()
+    print("mixer cleaned up")
+
+
 def load_new_sound():
     """reads resistance value of node in loading dock, and assigns
     an audio file to that node, basedf on selection from potentiometer"""
 
-GPIO.add_event_detect(4, GPIO.RISING, callback=play_step_sequencer, bouncetime=250)
+GPIO.add_event_detect(17, GPIO.RISING, callback=play_step_sequencer, bouncetime=250)
+GPIO.add_event_detect(27, GPIO.RISING, callback=end_step_sequencer, bouncetime=250)
+signal.signal(signal.SIGINT, signal_handler)
 
 
-play_step_sequencer()
+#play_step_sequencer()
